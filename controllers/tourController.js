021eb5310fc9f5bd41d298aec11c9c4a5e4ae0a9
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2022-11-18 13:01:31 +0300

    connectinng a hosted mongodb database to our local project

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,26 +1,28 @@
 const fs = require('fs');
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`))
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
 
 exports.checkID = (req, res, next, val) => {
   console.log(`Tour id is: ${val}`);
-  if (req.params.id * 1 > tours.length){
+  if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'Fail',
-      message: 'Invalid Id'
-    })
+      message: 'Invalid Id',
+    });
   }
-  next()
-}
+  next();
+};
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
     res.status(400).json({
-      status: "fail",
-      message: "Missing name or price"
-    })
+      status: 'fail',
+      message: 'Missing name or price',
+    });
   }
-  next()
-}
+  next();
+};
 
 exports.getAllTours = (req, res) => {
   console.log(req.reqTime);
@@ -29,56 +31,58 @@ exports.getAllTours = (req, res) => {
     requestedAt: req.reqTime,
     results: tours.length,
     data: {
-      tours: tours
-    }
-  })
-}
+      tours: tours,
+    },
+  });
+};
 
 //Route handlers to get a tour with a specific id
 exports.getTour = (req, res) => {
   console.log(req.params);
   const id = req.params.id * 1;
-  const tour = tours.find(el => el.id === id)
-
+  const tour = tours.find((el) => el.id === id);
 
   res.status(200).json({
     status: 'Success',
     data: {
-      tour
-    }
-  })
-}
+      tour,
+    },
+  });
+};
 
 //Route handlers to get create a tour
 exports.createTour = (req, res) => {
   // console.log(req.body);
-  const  newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({id: newId}, req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
 
   tours.push(newTour);
-  fs.writeFile(`${__dirname}/../dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-    res.status(201).json({
-      status: 'Success',
-      data: {
-        tour: newTour
-      }
-    })
-  })
+  fs.writeFile(
+    `${__dirname}/../dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'Success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 
   // res.send('done!')
-}
+};
 
 //Route handlers to update tour
 exports.updateTour = (req, res) => {
   res.status(204).json({
     status: 'success',
-    data: '<Updated Tour Herer.....>'
-  })
-}
+    data: '<Updated Tour Herer.....>',
+  });
+};
 exports.deleteTour = (req, res) => {
-
   res.status(204).json({
     status: 'success',
-    data: null
-  })
-}
+    data: null,
+  });
+};
--- server.js
+++ server.js
@@ -1,6 +1,20 @@
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config({path: './.env'})
 const app = require('./app')
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+mongoose.connect(DB,{
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(() => console.log("DB connecvtion successfull"))
+
+const toursSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  price: Number
+})
 
 
 
